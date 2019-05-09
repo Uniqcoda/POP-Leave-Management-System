abf7284808870aeb3b598d7fc18558ada24f6bed
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-09 23:02:16 +0100

    Refactor and clean up the code

--- database.test.js
+++ database.test.js
@@ -3,13 +3,13 @@ let Staff = require('./database').Staff;
 let Admin = require('./database').Admin;
 
 let ola = new Staff('Ola', 'ola@ola.com');
-ola.saveDetails();
+ola.save();
 let charles = new Admin('Charles', 'charles@charles.com');
-charles.saveDetails();
+charles.save();
 let cynthia = new Staff('Cynthia', 'cynth@more.com');
-cynthia.saveDetails();
+cynthia.save();
 let emeka = new Admin('Emeka', 'emeka@mekus.com');
-emeka.saveDetails();
+emeka.save();
 
 describe('Test cases for staff', function () {
     describe('Test for creating a staff', function () {
@@ -27,22 +27,22 @@ describe('Test cases for staff', function () {
     })
 });
 
-ola.makeRequest('1 week', 'annual');
-charles.makeRequest('1 month', 'sick');
-cynthia.makeRequest('2 months', 'maternity');
-ola.makeRequest('1 week', 'annual');
+ola.createRequest('20 days', 'annual');
+charles.createRequest('10 days', 'sick');
+cynthia.createRequest('60 days', 'maternity');
+charles.createRequest('20 days', 'annual');
 
 describe('Test cases for leave management', function () {
     describe('Test for staff making a leave request', function () {
         test('should check if the new request was created with the right details', function () {
-            let result = database.request[0];
+            let result = database.leaveRequest[0];
             expect(result).toHaveProperty('staffId', 1);
-            expect(result).toHaveProperty('duration', '1 week')
+            expect(result).toHaveProperty('duration', '20 days')
         })
     });
     describe('Test for staff reading own leave request', function () {
         test('should read all leave requests of staff', function () {
-            let result = ola.readOwnRequests();
+            let result = charles.readOwnRequests();
             expect(result.length).toBe(2)
         });
         test('should return a message if staff has no leave request record', function () {
@@ -74,19 +74,19 @@ describe('Test cases for leave management', function () {
 
     describe('Test for declining a leave request by id', function () {
         test('should add an approval property to a leave request and make it false', function () {
-            let result = emeka.declineRequest(4);
+            let result = emeka.rejectRequest(4);
             expect(result).toHaveProperty('isApproved', false);
         });
         test('should return a message if there is no request with such id', function () {
-            let result = emeka.declineRequest(25);
+            let result = emeka.rejectRequest(25);
             expect(result).toBe('id not found!');
         });
     });
 
     describe('Test for reading all leave requests by a staff', function () {
         test('should return all leave request by staff id', function () {
-            let result = charles.readAllRequestsByStaffId(1);
-            expect(result.length).toBe(2)
+            let result = emeka.readAllRequestsByStaffId(3);
+            expect(result.length).toBe(1)
         });
         test('should return a message if there is no staff with such id', function () {
             let result = charles.readAllRequestsByStaffId(25);
